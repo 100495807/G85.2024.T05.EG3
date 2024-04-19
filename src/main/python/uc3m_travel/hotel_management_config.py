738f0d00138d4557ca2c67efa51ac8c95ce50fe0
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-19 12:19:16 +0200

    Initial commit

---
+++ src/main/python/uc3m_travel/hotel_management_config.py
@@ -0,0 +1,4 @@
+"""Global constants for finding the path"""
+from pathlib import Path
+JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/G85.2024.T05.EG3/src/unittest/JSONFiles/"
+JSON_FILES_GUEST_ARRIVAL = JSON_FILES_PATH + "/guestarrival/"
