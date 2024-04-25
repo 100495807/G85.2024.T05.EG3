0a7411d44f78966eb5661db1ce635fd6e429b702
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-25 14:31:01 +0200

    singleton

--- src/main/python/uc3m_travel/storage/arrival_store.py
+++ src/main/python/uc3m_travel/storage/arrival_store.py
@@ -1,16 +1,26 @@
 from uc3m_travel.storage.json_store import JsonStore
 from uc3m_travel.hotel_management_exception import HotelManagementException
 
-class ArrivalStore(JsonStore):
-    def __init__(self):
-        super().__init__()
 
-    def check_arrival(self, file_input):
-        input_list = self.load_list_from_file(file_input)
-        # comprobar valores del fichero
-        try:
-            my_localizer = input_list["Localizer"]
-            my_id_card = input_list["IdCard"]
-        except KeyError as error:
-            raise HotelManagementException("Error - Invalid Key in JSON") from error
-        return my_id_card, my_localizer
+class ArrivalStore:
+    class __ArrivalStore(JsonStore):
+        def __init__(self):
+            super().__init__()
+
+        def check_arrival(self, file_input):
+            input_list = self.load_list_from_file(file_input)
+            # comprobar valores del fichero
+            try:
+                my_localizer = input_list["Localizer"]
+                my_id_card = input_list["IdCard"]
+            except KeyError as error:
+                raise HotelManagementException("Error - Invalid Key in JSON") from error
+            return my_id_card, my_localizer
+
+    __instance = None
+
+    def __new__(cls):
+        if not ArrivalStore.__instance:
+            ArrivalStore.__instance = ArrivalStore.__ArrivalStore()
+        # Add file path class attribute from inner class:
+        return ArrivalStore.__instance
