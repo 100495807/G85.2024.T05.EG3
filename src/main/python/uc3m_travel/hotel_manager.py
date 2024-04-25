50ae2a59f232b25eb3180841464001ee4530a4fa
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-25 13:48:44 +0200

    single_tone HM

--- src/main/python/uc3m_travel/hotel_manager.py
+++ src/main/python/uc3m_travel/hotel_manager.py
@@ -14,48 +14,53 @@ from uc3m_travel.storage.arrival_store import ArrivalStore
 class HotelManager:
     """Class with all the methods for managing reservations and stays"""
 
-    def __init__(self):
-        pass
+    class __HotelManager:
 
-    # pylint: disable=too-many-arguments
-    def room_reservation(self,
-                         credit_card: str,
-                         name_surname: str,
-                         id_card: str,
-                         phone_number: str,
-                         room_type: str,
-                         arrival_date: str,
-                         num_days: int) -> str:
-        """manges the hotel reservation: creates a reservation and saves it into a json file"""
-        my_reservation = HotelReservation(id_card=id_card,
-                                          credit_card_number=credit_card,
-                                          name_surname=name_surname,
-                                          phone_number=phone_number,
-                                          room_type=room_type,
-                                          arrival=arrival_date,
-                                          num_days=num_days)
+        # pylint: disable=too-many-arguments
+        @staticmethod
+        def room_reservation(credit_card: str,
+                             name_surname: str,
+                             id_card: str,
+                             phone_number: str,
+                             room_type: str,
+                             arrival_date: str,
+                             num_days: int) -> str:
+            """manges the hotel reservation: creates a reservation and saves it into a json file"""
+            my_reservation = HotelReservation(id_card=id_card,
+                                              credit_card_number=credit_card,
+                                              name_surname=name_surname,
+                                              phone_number=phone_number,
+                                              room_type=room_type,
+                                              arrival=arrival_date,
+                                              num_days=num_days)
 
-        ReservationStore().add_item(my_reservation)
+            ReservationStore().add_item(my_reservation)
 
-        return my_reservation.localizer
+            return my_reservation.localizer
 
+        @staticmethod
+        def guest_arrival(file_input: str) -> str:
+            """manages the arrival of a guest with a reservation"""
+            my_checkin = HotelStay.create_guest_arrival_from_file(file_input)
 
-    def guest_arrival(self, file_input: str) -> str:
-        """manages the arrival of a guest with a reservation"""
-        my_checkin = HotelStay.create_guest_arrival_from_file(file_input)
+            CheckInStore().add_item(my_checkin.__dict__)
 
-        CheckInStore().add_item(my_checkin.__dict__)
+            return my_checkin.room_key
 
-        return my_checkin.room_key
+        @staticmethod
+        def guest_checkout(room_key: str) -> bool:
+            """manages the checkout of a guest"""
+            room_key = RoomKey(room_key).value
+            departure_date_timestamp = CheckInStore().check_checkin(room_key)
+            today = datetime.utcnow().date()
+            if datetime.fromtimestamp(departure_date_timestamp).date() != today:
+                raise HotelManagementException("Error: today is not the departure day")
+            CheckOutStore().add_item(room_key)
+            return True
 
+    __instance = None
 
-
-    def guest_checkout(self, room_key: str) -> bool:
-        """manages the checkout of a guest"""
-        room_key = RoomKey(room_key).value
-        departure_date_timestamp = CheckInStore().check_checkin(room_key)
-        today = datetime.utcnow().date()
-        if datetime.fromtimestamp(departure_date_timestamp).date() != today:
-            raise HotelManagementException("Error: today is not the departure day")
-        CheckOutStore().add_item(room_key)
-        return True
+    def __new__(cls):
+        if not HotelManager.__instance:
+            HotelManager.__instance = HotelManager.__HotelManager()
+        return HotelManager.__instance
