47fca52fd4bb00cfb986846f642f647dd4aaea1c
100495807 <158489101+100495807@users.noreply.github.com>
2024-04-25 14:31:08 +0200

    singleton

--- src/main/python/uc3m_travel/storage/checkout_store.py
+++ src/main/python/uc3m_travel/storage/checkout_store.py
@@ -3,15 +3,27 @@ from uc3m_travel.hotel_management_config import JSON_FILES_PATH
 from uc3m_travel.hotel_management_exception import HotelManagementException
 from datetime import datetime
 
-class CheckOutStore(JsonStore):
-    _file_name = JSON_FILES_PATH + "store_check_out.json"
-    def __init__(self):
-        super().__init__()
 
-    def add_item(self, item):
-        """add_item para reservation_store"""
-        check_out_found = self.find_item("room_key", item)
-        if check_out_found:
-            raise HotelManagementException("Guest is already out")
-        room_checkout = {"room_key": item, "checkout_time": datetime.timestamp(datetime.utcnow())}
-        super().add_item(room_checkout)
+class CheckOutStore:
+
+    class __CheckOutStore(JsonStore):
+        _file_name = JSON_FILES_PATH + "store_check_out.json"
+
+        def __init__(self):
+            super().__init__()
+
+        def add_item(self, item):
+            """add_item para reservation_store"""
+            check_out_found = self.find_item("room_key", item)
+            if check_out_found:
+                raise HotelManagementException("Guest is already out")
+            room_checkout = {"room_key": item, "checkout_time": datetime.timestamp(datetime.utcnow())}
+            super().add_item(room_checkout)
+
+    __instance = None
+
+    def __new__(cls):
+        if not CheckOutStore.__instance:
+            CheckOutStore.__instance = CheckOutStore.__CheckOutStore()
+        # Add file path class attribute from inner class:
+        return CheckOutStore.__instance
